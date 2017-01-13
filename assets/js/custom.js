ead476c27b4899f227caff02f49624f0745534da
Gonzalo Terzano <gonzalo.terzano@globant.com>
2017-01-13 14:38:02 -0300

    testing google analytics 2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -241,15 +241,6 @@
                 });
         });
 
-        // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        //     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        //     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        // ga('create', 'UA-90335309-1', 'auto');
-        // ga('send', 'pageview');
-
-
     });
 
     function autoWrite(querySelector, text) {
@@ -301,3 +292,11 @@
 })(jQuery);
 
 
+/***GOOGLE ANALYTICS***/
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-90335309-1', 'auto');
+ga('send', 'pageview');
